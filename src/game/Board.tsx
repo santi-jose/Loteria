408cbf3fa6cd9370078bb0142fe01f46f4f58037
jlsantia21 <jlsantia21@gmail.com>
2025-10-03 01:29:22 -0700

    Added type WinType and WinPattern to assist with checking for winning patterns in Board class. WinType is a union of strings, being used the same way an enum would. It stores the different winning types accepted in Loteria (s.a. row and column). WinPattern is a type which groups three fields that are necessary to check for the win condition at the Game Manager class level. It includes a WinType field to depict the win type of the winning pattern, a positions array of pairs, which depicts the indices of the cards used to fulfill the win condition, and an array of Card's which is the actual Card objects which are used to fulfill the win condition.  Added allPatterns static class field which depicts all possible winning patterns of all different win conditions. This static variable will be used to check against the current Board state and return an array of WinPattern type objects.

--- src/game/Board.tsx
+++ src/game/Board.tsx
@@ -1,8 +1,55 @@
 import { BoardTile } from "./BoardTile";
 import { Deck } from "./Deck";
+import { Card } from "./Card"
+
+// string union used to denote the type of win condition met on the Board
+type WinType = "ROW" | "COLUMN" | "DIAGONAL" | "POZO" | "FULL";
+
+// WinPattern object which depicts the winning pattern on the board
+// includes the WinType, positions, and Cards used in the win pattern
+type WinPattern = {
+    winType: WinType;
+    positions: [number, number][];
+    cards: Card[];
+}
+
 
 export class Board{
     private grid: BoardTile[][];
+
+    // static variable storing all different winning patterns to check
+    // in the board state
+    private static allPatterns: WinPattern[] = [
+        // ROW
+        {winType: "ROW", positions: [[0, 0], [0, 1], [0, 2], [0, 3]], cards: [] },
+        {winType: "ROW", positions: [[1, 0], [1, 1], [1, 2], [1, 3]], cards: [] },
+        {winType: "ROW", positions: [[2, 0], [2, 1], [2, 2], [2, 3]], cards: [] },
+        {winType: "ROW", positions: [[3, 0], [3, 1], [3, 2], [3, 3]], cards: [] },
+        // COLUMN
+        {winType: "COLUMN", positions: [[0, 0], [1, 0], [2, 0], [3, 0]], cards: [] },
+        {winType: "COLUMN", positions: [[0, 1], [1, 1], [2, 1], [3, 1]], cards: [] },
+        {winType: "COLUMN", positions: [[0, 2], [1, 2], [2, 2], [3, 2]], cards: [] },
+        {winType: "COLUMN", positions: [[0, 3], [1, 3], [2, 3], [3, 3]], cards: [] },
+        // DIAGONAL
+        {winType: "DIAGONAL", positions: [[0, 0], [1, 1], [2, 2], [3, 3]], cards: [] },
+        {winType: "DIAGONAL", positions: [[0, 3], [1, 2], [2, 1], [3, 0]], cards: [] },
+        // POZO
+        {winType: "POZO", positions: [[0, 0], [0, 1], [1, 0], [1, 1]], cards: [] },
+        {winType: "POZO", positions: [[0, 1], [0, 2], [1, 1], [1, 2]], cards: [] },
+        {winType: "POZO", positions: [[0, 2], [0, 3], [1, 2], [1, 3]], cards: [] },
+        {winType: "POZO", positions: [[1, 0], [1, 1], [2, 0], [2, 1]], cards: [] },
+        {winType: "POZO", positions: [[1, 1], [1, 2], [2, 1], [2, 2]], cards: [] },
+        {winType: "POZO", positions: [[1, 2], [1, 3], [2, 2], [2, 3]], cards: [] },
+        {winType: "POZO", positions: [[2, 0], [2, 1], [3, 0], [3, 1]], cards: [] },
+        {winType: "POZO", positions: [[2, 1], [2, 2], [3, 1], [3, 2]], cards: [] },
+        {winType: "POZO", positions: [[2, 2], [2, 3], [3, 2], [3, 3]], cards: [] },
+        // FULL
+        {winType: "FULL", positions: [[0, 0], [0, 1], [0, 2], [0, 3],
+                                      [1, 0], [1, 1], [1, 2], [1, 3],
+                                      [2, 0], [2, 1], [2, 2], [2, 3],
+                                      [3, 0], [3, 1], [3, 2], [3, 3]],
+                                    cards: [] }
+    ];
     
     constructor(){
         // initialize column array 
@@ -22,5 +69,11 @@ export class Board{
         }
     }
 
-    
+    // function checks for winning patterns on the board
+    // returns an array of WinPattern objects
+    checPatterns(): WinPattern[]{
+
+        return [];
+    }
 }
+
