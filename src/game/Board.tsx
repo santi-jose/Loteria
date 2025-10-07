70d35836c572685ef94fff40fa21fff48dca22f0
jlsantia21 <jlsantia21@gmail.com>
2025-10-07 00:50:56 -0700

    Completed Board class implementation. Completed checkPattern() function which uses a static private variable, allPatterns, to check if the current Board state has any winning patterns. The function returns an array of type WinPattern, which contains a WinType defining what the win condition fulfilled is (row, column, etc.), indices array, which is an array of indices that show the position of the cards in the pattern, and a cards array, which tells us the cards that are used to fulfill this WinPattern condition. The toggle function was also completed. It defines a function which toggles the boolean value within the BoardTile class which tells us whether a tile is marked. The function calls the toggle function defined in the BoardTile class, given inputs of i and j which depict the row and column indices of the tile to be toggled.

--- src/game/Board.tsx
+++ src/game/Board.tsx
@@ -1,79 +1,301 @@
 import { BoardTile } from "./BoardTile";
 import { Deck } from "./Deck";
-import { Card } from "./Card"
+import { Card } from "./Card";
 
 // string union used to denote the type of win condition met on the Board
 type WinType = "ROW" | "COLUMN" | "DIAGONAL" | "POZO" | "FULL";
 
 // WinPattern object which depicts the winning pattern on the board
-// includes the WinType, positions, and Cards used in the win pattern
+// includes the WinType, indices, and Cards used in the win pattern
 type WinPattern = {
-    winType: WinType;
-    positions: [number, number][];
-    cards: Card[];
-}
+  winType: WinType;
+  indices: [number, number][];
+  cards: Card[];
+};
 
+export class Board {
+  private grid: BoardTile[][];
 
-export class Board{
-    private grid: BoardTile[][];
+  // static variable storing all different winning patterns to check
+  // in the board state
+  private static allPatterns: WinPattern[] = [
+    // ROW
+    {
+      winType: "ROW",
+      indices: [
+        [0, 0],
+        [0, 1],
+        [0, 2],
+        [0, 3],
+      ],
+      cards: [],
+    },
+    {
+      winType: "ROW",
+      indices: [
+        [1, 0],
+        [1, 1],
+        [1, 2],
+        [1, 3],
+      ],
+      cards: [],
+    },
+    {
+      winType: "ROW",
+      indices: [
+        [2, 0],
+        [2, 1],
+        [2, 2],
+        [2, 3],
+      ],
+      cards: [],
+    },
+    {
+      winType: "ROW",
+      indices: [
+        [3, 0],
+        [3, 1],
+        [3, 2],
+        [3, 3],
+      ],
+      cards: [],
+    },
+    // COLUMN
+    {
+      winType: "COLUMN",
+      indices: [
+        [0, 0],
+        [1, 0],
+        [2, 0],
+        [3, 0],
+      ],
+      cards: [],
+    },
+    {
+      winType: "COLUMN",
+      indices: [
+        [0, 1],
+        [1, 1],
+        [2, 1],
+        [3, 1],
+      ],
+      cards: [],
+    },
+    {
+      winType: "COLUMN",
+      indices: [
+        [0, 2],
+        [1, 2],
+        [2, 2],
+        [3, 2],
+      ],
+      cards: [],
+    },
+    {
+      winType: "COLUMN",
+      indices: [
+        [0, 3],
+        [1, 3],
+        [2, 3],
+        [3, 3],
+      ],
+      cards: [],
+    },
+    // DIAGONAL
+    {
+      winType: "DIAGONAL",
+      indices: [
+        [0, 0],
+        [1, 1],
+        [2, 2],
+        [3, 3],
+      ],
+      cards: [],
+    },
+    {
+      winType: "DIAGONAL",
+      indices: [
+        [0, 3],
+        [1, 2],
+        [2, 1],
+        [3, 0],
+      ],
+      cards: [],
+    },
+    // POZO
+    {
+      winType: "POZO",
+      indices: [
+        [0, 0],
+        [0, 1],
+        [1, 0],
+        [1, 1],
+      ],
+      cards: [],
+    },
+    {
+      winType: "POZO",
+      indices: [
+        [0, 1],
+        [0, 2],
+        [1, 1],
+        [1, 2],
+      ],
+      cards: [],
+    },
+    {
+      winType: "POZO",
+      indices: [
+        [0, 2],
+        [0, 3],
+        [1, 2],
+        [1, 3],
+      ],
+      cards: [],
+    },
+    {
+      winType: "POZO",
+      indices: [
+        [1, 0],
+        [1, 1],
+        [2, 0],
+        [2, 1],
+      ],
+      cards: [],
+    },
+    {
+      winType: "POZO",
+      indices: [
+        [1, 1],
+        [1, 2],
+        [2, 1],
+        [2, 2],
+      ],
+      cards: [],
+    },
+    {
+      winType: "POZO",
+      indices: [
+        [1, 2],
+        [1, 3],
+        [2, 2],
+        [2, 3],
+      ],
+      cards: [],
+    },
+    {
+      winType: "POZO",
+      indices: [
+        [2, 0],
+        [2, 1],
+        [3, 0],
+        [3, 1],
+      ],
+      cards: [],
+    },
+    {
+      winType: "POZO",
+      indices: [
+        [2, 1],
+        [2, 2],
+        [3, 1],
+        [3, 2],
+      ],
+      cards: [],
+    },
+    {
+      winType: "POZO",
+      indices: [
+        [2, 2],
+        [2, 3],
+        [3, 2],
+        [3, 3],
+      ],
+      cards: [],
+    },
+    // FULL
+    {
+      winType: "FULL",
+      indices: [
+        [0, 0],
+        [0, 1],
+        [0, 2],
+        [0, 3],
+        [1, 0],
+        [1, 1],
+        [1, 2],
+        [1, 3],
+        [2, 0],
+        [2, 1],
+        [2, 2],
+        [2, 3],
+        [3, 0],
+        [3, 1],
+        [3, 2],
+        [3, 3],
+      ],
+      cards: [],
+    },
+  ];
 
-    // static variable storing all different winning patterns to check
-    // in the board state
-    private static allPatterns: WinPattern[] = [
-        // ROW
-        {winType: "ROW", positions: [[0, 0], [0, 1], [0, 2], [0, 3]], cards: [] },
-        {winType: "ROW", positions: [[1, 0], [1, 1], [1, 2], [1, 3]], cards: [] },
-        {winType: "ROW", positions: [[2, 0], [2, 1], [2, 2], [2, 3]], cards: [] },
-        {winType: "ROW", positions: [[3, 0], [3, 1], [3, 2], [3, 3]], cards: [] },
-        // COLUMN
-        {winType: "COLUMN", positions: [[0, 0], [1, 0], [2, 0], [3, 0]], cards: [] },
-        {winType: "COLUMN", positions: [[0, 1], [1, 1], [2, 1], [3, 1]], cards: [] },
-        {winType: "COLUMN", positions: [[0, 2], [1, 2], [2, 2], [3, 2]], cards: [] },
-        {winType: "COLUMN", positions: [[0, 3], [1, 3], [2, 3], [3, 3]], cards: [] },
-        // DIAGONAL
-        {winType: "DIAGONAL", positions: [[0, 0], [1, 1], [2, 2], [3, 3]], cards: [] },
-        {winType: "DIAGONAL", positions: [[0, 3], [1, 2], [2, 1], [3, 0]], cards: [] },
-        // POZO
-        {winType: "POZO", positions: [[0, 0], [0, 1], [1, 0], [1, 1]], cards: [] },
-        {winType: "POZO", positions: [[0, 1], [0, 2], [1, 1], [1, 2]], cards: [] },
-        {winType: "POZO", positions: [[0, 2], [0, 3], [1, 2], [1, 3]], cards: [] },
-        {winType: "POZO", positions: [[1, 0], [1, 1], [2, 0], [2, 1]], cards: [] },
-        {winType: "POZO", positions: [[1, 1], [1, 2], [2, 1], [2, 2]], cards: [] },
-        {winType: "POZO", positions: [[1, 2], [1, 3], [2, 2], [2, 3]], cards: [] },
-        {winType: "POZO", positions: [[2, 0], [2, 1], [3, 0], [3, 1]], cards: [] },
-        {winType: "POZO", positions: [[2, 1], [2, 2], [3, 1], [3, 2]], cards: [] },
-        {winType: "POZO", positions: [[2, 2], [2, 3], [3, 2], [3, 3]], cards: [] },
-        // FULL
-        {winType: "FULL", positions: [[0, 0], [0, 1], [0, 2], [0, 3],
-                                      [1, 0], [1, 1], [1, 2], [1, 3],
-                                      [2, 0], [2, 1], [2, 2], [2, 3],
-                                      [3, 0], [3, 1], [3, 2], [3, 3]],
-                                    cards: [] }
-    ];
-    
-    constructor(){
-        // initialize column array 
-        this.grid = [];
-        const tempDeck = new Deck();
-        tempDeck.shuffle();
+  constructor() {
+    // initialize column array
+    this.grid = [];
+    const tempDeck = new Deck();
+    tempDeck.shuffle();
 
-        // iterate through rows
-        for(let i: number = 0; i < 4; i++){
-            this.grid[i] = []; // define row to iterate
-            
-            // iterate through row elements
-            for(let j: number = 0; j < 4; j++){
-                // assign BoardTile to grid space i,j
-                this.grid[i][j] = new BoardTile(tempDeck.draw()!);
-            }
+    // iterate through rows
+    for (let i: number = 0; i < 4; i++) {
+      this.grid[i] = []; // define row to iterate
+
+      // iterate through row elements
+      for (let j: number = 0; j < 4; j++) {
+        // assign BoardTile to grid space i,j
+        this.grid[i][j] = new BoardTile(tempDeck.draw()!);
+      }
+    }
+  }
+
+  // function checks for winning patterns on the board
+  // returns an array of WinPattern objects
+  checkPatterns(): WinPattern[] {
+    // initialize empty WinPattern array to return
+    let winningPatterns: WinPattern[] = [];
+
+    // iterate through the winning patterns inside allPatterns array
+    for (const winPattern of Board.allPatterns) {
+      // define WinPattern to use to store current winning pattern
+      const currentPattern: WinPattern = {
+        winType: winPattern.winType,
+        indices: winPattern.indices,
+        cards: [],
+      };
+
+      // iterate through pattern indices
+      const indices = winPattern.indices;
+      let allMarked = true; // flag which tells us if we have a winning pattern
+      for (const index of indices) {
+        const tile = this.grid[index[0]][index[1]];
+        // check if tile  at given indices in grid is not marked
+        if (!tile.isMarked) {
+          allMarked = false;
+          break; // break if so
         }
-    }
+        currentPattern.cards.push(tile.Card); // otherwise  push current Card
+        // into the currentPattern WinPattern
+      }
 
-    // function checks for winning patterns on the board
-    // returns an array of WinPattern objects
-    checPatterns(): WinPattern[]{
-
-        return [];
+      // if allMarked is still true
+      if (allMarked) {
+        winningPatterns.push(currentPattern); // push currentPattern onto winningPatterns array
+      }
     }
+    return winningPatterns;
+  }
+
+  // toggle the current tile marked state given the indices of the tile
+  // i: row, j: column
+  toggle(i: number, j: number): void{
+    this.grid[i][j].toggle();
+  }
 }
-
