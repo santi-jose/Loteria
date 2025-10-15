bf763b83d6e25a1d3ccbc8b2408eead17e126220
jlsantia21 <jlsantia21@gmail.com>
2025-10-15 01:05:02 -0700

    Changed how testing the initialization of Board is done. Instead of logging the Card data, we use assertions to ensure the data is being properly initialized. We also modified the toggle function test to better capture the behavior of the toggle function instead of explicitly testing the different coordinates.

--- src/tests/Board.test.tsx
+++ src/tests/Board.test.tsx
@@ -5,39 +5,38 @@ import { Board } from '../game/Board';
 describe('Board', () => {
     it('initializes a Board with 16 different unmarked cards', () => {
         const board = new Board();
+        const seenIDs = new Set<number>();
 
         // iterate through rows
         for(let i = 0; i < 4; i++){
             // iterate through columns
             for(let j = 0; j < 4; j++){
-                console.log(`i: ${i}, j: ${j}`);
-                console.log(board.Tile(i, j).Card.Name);
-                console.log(board.Tile(i, j).Card.ID)
-                console.log(board.Tile(i, j).Card.Description);
-                console.log(board.Tile(i, j).Card.Image);
-                expect(board.Tile(i,j).isMarked).toBe(false);
+                // store tile to test
+                const tile = board.getTile(i, j);
+
+                expect(tile.isMarked).toBe(false);
+                expect(tile.Card).toBeDefined();
+                expect(tile.Card.Name).toBeTypeOf('string');
+                expect(tile.Card.Description).toBeTypeOf('string');
+                expect(tile.Card.ID).toBeTypeOf('number');
+
+                // check for duplicate cards
+                expect(seenIDs.has(tile.Card.ID)).toBe(false);
+                seenIDs.add(tile.Card.ID);
             }
         }
+
+        expect(seenIDs.size).toBe(16);
     });
     it('toggles a BoardTile given the row and column indices as input (i, j)', () => {
         const board = new Board();
 
-        // check that board is unmarked at indices to be toggled
-        expect(board.isMarked(0, 0)).toBe(false);
-        expect(board.isMarked(1, 1)).toBe(false);
-        expect(board.isMarked(2, 2)).toBe(false);
-        expect(board.isMarked(3, 3)).toBe(false);
-
-        // mark tiles (0, 0) (1, 1) (2, 2) and (3, 3)
-        board.toggle(0, 0);
-        board.toggle(1, 1);
-        board.toggle(2, 2);
-        board.toggle(3, 3);
-
-        // check that board is marked at toggled indices
-        expect(board.isMarked(0, 0)).toBe(true);
-        expect(board.isMarked(1, 1)).toBe(true);
-        expect(board.isMarked(2, 2)).toBe(true);
-        expect(board.isMarked(3, 3)).toBe(true);
+        [[0, 0], [1, 1], [2, 2], [3, 3]].forEach(([i, j]) =>{
+            expect(board.isMarked(i, j)).toBe(false);
+            board.toggle(i, j);
+            expect(board.isMarked(i,j )).toBe(true);
+            board.toggle(i, j);
+            expect(board.isMarked(i, j)).toBe(false);
+        });
     });
 });
