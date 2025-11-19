91c05eb73c192ee3425bea728ff3e534303b11d1
jlsantia21 <jlsantia21@gmail.com>
2025-11-19 00:23:41 -0800

    Completed elementary testing of all functions for AIPLayer. All functions working as intended.

--- src/tests/AIPlayer.test.tsx
+++ src/tests/AIPlayer.test.tsx
@@ -30,10 +30,6 @@ describe('AIPlayer', () => {
             salo.attemptMark(card, 10);
         }
 
-        // Loteria call should be false
-        expect(salo.CalledLoteria).toBe(false);
-        expect(salo.Board.checkPatterns().length).toBe(0);
-
         // advance all timers
         vi.runAllTimers();
 
@@ -41,6 +37,9 @@ describe('AIPlayer', () => {
         for(let j = 0; j < 4; j++){
             expect(salo.Board.getTile(0, j).isMarked).toBe(true);
         }
+
+        // one win pattern should be recorded after marking row0
+        expect(salo.Board.checkPatterns().length).toBe(1);
 
         vi.useRealTimers();
     });
@@ -74,6 +73,44 @@ describe('AIPlayer', () => {
         // Loteria call now true, but winpattern should have 1
         expect(salo.CalledLoteria).toBe(true);
         expect(salo.Board.checkPatterns().length).toBe(1);
+
+        vi.useRealTimers();
+    });
+
+    it("Checks that playRound() function properly calls attemptMark and attemptLoteria().", () => {
+        // use fake timers
+        vi.useFakeTimers();
+
+        // initialize AIPlayer
+        // 100 % mark accuracy
+        // 100 % of pace time taken to mark Board
+        // 0 & Loteria Accuracy
+        // 100% of pace time taken to call Loteria
+        const salo = new AIPlayer('Salo', 100, 100, 100, 100);
+        const row0 = [];
+
+        // create array of row0
+        for(let j = 0; j < 4; j++){
+            row0.push(salo.Board.getTile(0, j).Card);
+        }
+
+        for(const card of row0){
+            salo.playRound(card, 10);
+        }
+
+        vi.runAllTimers();
+
+        // check that the AIPlayer's board was marked in the right places
+        for(let j = 0; j < 4; j++){
+            expect(salo.Board.getTile(0, j).isMarked).toBe(true);
+        }
+
+        // salo.playRound(row0[0], 10);
+        // vi.runAllTimers();
+
+        // LoteriaAccuracy = 0 -> calledLoteria flag is false
+        expect(salo.Board.checkPatterns().length).toBe(1);
+        expect(salo.CalledLoteria).toBe(true);
 
         vi.useRealTimers();
     });
