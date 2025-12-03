7d630181a9ac48234a7a8c9c7de88f77e80be4e1
jlsantia21 <jlsantia21@gmail.com>
2025-12-03 01:45:24 -0800

    Rewrote all state machine from C style to JS style event driven state transitions. Added timer member variables to help with pausing and resuming game during Loteria validation. Added Callback function definition for players to use when they call Loteria, allowing them to flag a change in state from playRound to validation. Added timer pause during validation and storage of essential values to continue playing round after an invalid call to loteria.

--- src/game/GameManager.tsx
+++ src/game/GameManager.tsx
@@ -11,6 +11,10 @@ export class GameManager{
     private config: GameConfig;
     private currentCard!: Card;
     private winner: Player | undefined =  undefined;
+    // timer member variables
+    private playTimer: NodeJS.Timeout | null = null;
+    private playStartTime =  0;
+    private remainingPlayTime = 0;
 
     constructor(config: GameConfig){
         this.state = "standby";
@@ -19,7 +23,7 @@ export class GameManager{
         this.dealer.Deck.shuffle(); // shuffle deck
         
         // create and push user player into Player array
-        this.players.push(new Player("Player 1"))
+        this.players.push(new Player("Player 1"));
 
         // initialize AIPlayers with random values
         // and push onto the Players array
@@ -31,85 +35,102 @@ export class GameManager{
                               this.randomPercent()
                               ));                 
         }
+
+        // Initialize callback function for player loteria call
+        for(const player of this.players){
+            player.onLoteria = () => {
+                console.log(`${player.Name} called LOTERIA`);
+                this.validateLoteria(player);
+            };
+        }
     }
 
-    gameLoop(): void{
-        switch(this.state){
-            // standby phase where we draw the card for the round. 
-            case "standby":
-                const currentCard = this.dealer.announceCard();
-                console.log(`Current Card in round: ${currentCard}`);
+    enterStandby(): void{
+        this.state = "standby";
+        console.log("State = standby");
+        const currentCard = this.dealer.announceCard();
 
-                // if we receive undefined
-                // we ran out of cards
-                if(currentCard === undefined){
-                    this.state = "gameover"; // set state to gameover
-                    break; // break out of case play
-                }
-                this.currentCard = currentCard; // save currentCard to GameManager private variable
+        // check for undefined currentcard
+        if(currentCard === undefined){
+            this.enterGameOver();
+            return;
+        }
+        this.currentCard = currentCard;
+        console.log(`Current Card in round: ${this.currentCard?.Name}`);
+        
+        this.enterPlay();
+    }
 
-                // queue next standby phase
-                setTimeout( () => {
-                    this.state = "standby";
-                }, this.config.Pace * 1000);
+    enterPlay(): void{
+        this.state = "play";
+        console.log("State = play");
 
-                this.state = "play"; // set state to play
-                break;
-            case "play":
-                // play round
-                
-                // iterate through AIPlayers and play a round
-                for(const player of this.players){
-                    if(player instanceof AIPlayer){
-                        player.playRound(this.currentCard, this.config.Pace);
-                    }
-                }
+        this.playStartTime = Date.now();
 
-                // poll for the Loteria calls of players
-                for(const player of this.players){
-                    if(player.CalledLoteria){
-                        // call function to validate loteria
-                        this.validateLoteria(player);
-                    }
-                }
-                break;
-            case "gameover":
-                if(this.winner === undefined){ // if we have no winner
-                    console.log(`Game Over. Deck ran out before a winner could be decided. Tie game!`);
-                }else{
-                    console.log(`Game Over. Player ${this.winner.Name} wins!`);
-                }
-                break;
+        // start timer for transition to standby
+        this.playTimer = setTimeout(() => {
+            this.enterStandby();
+        }, this.config.Pace * 1000);
+
+        // trigger AIPlayers to play round
+        for(const player of this.players){
+            if(player instanceof AIPlayer){
+                player.playRound(this.currentCard, this.config.Pace);
+            }
+        }
+    }
+
+    enterGameOver(): void{
+        this.state = "gameover";
+        console.log("State = gameover");
+        if(this.winner === undefined){
+            console.log(`Game Over. Deck ran out before a winner could be decided. Tie game!`);
+        }else{
+            console.log(`Game Over. Player ${this.winner.Name} wins!`);
         }
     }
 
     validateLoteria(player: Player): void{
+        // ignore late calls
+        if(this.state!=="play") return;
+        console.log(`Validating Loteria for ${player.Name}...`);
+
+        // Pause play timer
+        if(this.playTimer){
+            clearTimeout(this.playTimer);
+            this.playTimer = null;
+        }
+
+        // calculate how much time was left in the round
+        const elapsed = (Date.now() - this.playStartTime) / 1000;
+        this.remainingPlayTime = this.config.Pace - elapsed;
+
+        let validLoteria = true;
         // iterate through array of patterns within player Board
         for(const pattern of player.Board.checkPatterns()){
-            let valid = true;
             for(const card of pattern.cards){
                 // if the card is not included in the cards seen deck 
                 if(!this.dealer.CardsDrawn.Cards.includes(card)){
                     // current pattern is not valid
-                    valid = false;
+                    validLoteria = false;
                     break; // break to check next pattern
                 }
             }
             // if the full pattern exists in the CardsDrawn deck
-            if(valid === true){
+            if(validLoteria === true){
                 this.winner = player; // save winner to 
-                this.state = "gameover"; // go to state gameover
-                break;
+                this.enterGameOver();
             }
         }
-        // if we make it out of the loop without going to state gameover
-        if(this.state !== "gameover"){
+        // if we make it out of the loop without confirming validLoteria
+        if(!validLoteria){
              // incur penalty for player
-            console.log("Invalid Loteria, Penalty applied.");
+            console.log(`Invalid Loteria call by ${player.Name}, Penalty applied.`);
             // apply penalty to random tile in palyer's board
              const i = this.randomIndex();
              const j = this.randomIndex();
              player.Board.getTile(i, j).deactivate();
+             this.resumePlayRound();
         }
     }
 
@@ -121,5 +142,21 @@ export class GameManager{
     // helper function to return a random index value from 0 to 3
     private randomIndex(): number{
         return Math.floor((Math.random() * 4)); 
+    }
+
+    // helper to resume playRound after invalid Loteria call
+    private resumePlayRound(): void{
+        if(this.remainingPlayTime <= 0){
+            // Round ended during pause -> go to standby
+            this.enterStandby();
+            return;
+        }
+
+        this.playStartTime = Date.now();
+
+        this.playTimer = setTimeout(() => {
+            this.enterStandby();
+        }, this.remainingPlayTime * 1000);
+        console.log(`Play resumed for ${this.remainingPlayTime.toFixed(2)} seconds`);
     }
 }
