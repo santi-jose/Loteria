723bd0fc3dbbb68ffc5dc4d76f128920351c6d07
jlsantia21 <jlsantia21@gmail.com>
2026-02-10 00:42:38 -0800

    Completed initial AI overlay implementations. A lot of repeated code. I feel like this can be optimized. I tried using optional props for the AIOverlay implementation since they don't need any interactivity built into them, but I was having a hard time since I didn't define the type of the prop to be undefined. I can go back and fix this. But another thing that came up at the end was that I am using the CardToggle prop which requires a function and is clickable. This is funcitonality that is not necessary for the AIOverlay since the user cannot interact with it. This AIOverlay board needs to use the CardState we previously defined in our component extraction design step. There are a few things we need to optimize for the AIOverlay.

--- src/components/gameplay/overlays/AIOverlay.tsx
+++ src/components/gameplay/overlays/AIOverlay.tsx
@@ -1,7 +1,23 @@
-export default function AIOverlay(){
+import Board from "./board/Board";
+import { BoardTile } from "../../../game/BoardTile";
+
+type AIOverlayProps = {
+    id: number;
+    rowArray: BoardTile[][];
+    cardToggleGrid: boolean[][];
+    activeCardToggleGrid: boolean[][];
+}
+
+export default function AIOverlay({id, rowArray, cardToggleGrid, activeCardToggleGrid}: AIOverlayProps){
     return(
-        <>
-            AIOverlay
-        </>
+        <div>
+            {`AI${id}Overlay`}
+            <Board 
+                rowArray={rowArray}
+                cardToggleGrid={cardToggleGrid}
+                activeCardToggleGrid={activeCardToggleGrid}
+                onToggleCard={()=>(console.log('AIPlayer has no toggle'))}
+            />
+        </div>
     );
 }
